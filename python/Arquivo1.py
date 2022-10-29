6ed2d0728570202a340ed1d4ef9818169c7f20e6
Amanda Cornelsen <cornelsen089@gmail.com>
2022-10-28 21:24:11 -0300

    VerificadorDeIdade

---
+++ python/Arquivo1.py
@@ -0,0 +1,44 @@
+diaAtual = 28
+diaemmes = diaAtual/30
+
+
+
+mesAtual = 10
+mesAtual = mesAtual + diaemmes
+mesemano = mesAtual/12
+
+
+
+anoAtual = 2022
+anoAtual = anoAtual + mesemano
+
+
+
+dia = input("put your birthday  ")
+
+mes = input("put your month  ")
+
+ano = input("put your year  ") 
+
+diaemmes = int(dia)/30
+
+mesemano = (int(mes) + diaemmes)/12
+
+ano = int(ano) + mesemano
+
+idade = anoAtual - ano
+
+
+
+
+
+
+
+
+if idade >= 18:
+
+    print("congratulation,your access is complete")
+
+else : 
+    print("i'm so sorry, but you are not admit in our house")
+    
